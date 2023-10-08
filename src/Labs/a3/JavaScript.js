662a9f98a87dff903b2fd931618f15fe65e13fae
John-C-Kuang <kuang.che@northeastern.edu>
2023-10-07 21:57:36 -0400

    lab3

---
+++ src/Labs/a3/JavaScript.js
@@ -0,0 +1,48 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import WorkingWithFunctions from "./functions/WorkingWithFunctions";
+import WorkingWithArrays from "./arrays/WorkingWithArrays";
+import ArrayIndexAndLength from "./arrays/ArrayIndexAndLength";
+import AddingAndRemovingDataToFromArrays
+  from "./arrays/AddingAndRemovingDataToFromArrays";
+import ForLoops from "./arrays/ForLoops";
+import MapFunction from "./MapFunction";
+import JsonStringify from "./JsonStringify";
+import FindFunction from "./FindFunction";
+import FilterFunction from "./FilterFunction";
+import TemplateLiterals from "./TemplateLiterals";
+import House from "./House";
+import Spread from "./Spread";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+
+function JavaScript() {
+  return(
+      <div>
+            <h1>JavaScript</h1>
+            <VariablesAndConstants/>
+            <VariableTypes />
+            <BooleanVariables/>
+            <IfElse />
+            <TernaryOperator />
+            <WorkingWithFunctions />
+            <WorkingWithArrays />
+            <ArrayIndexAndLength />
+            <AddingAndRemovingDataToFromArrays />
+            <ForLoops />
+            <MapFunction />
+            <JsonStringify />
+            <FindFunction />
+            <FilterFunction />
+            <TemplateLiterals />
+            <House />
+            <Spread />
+            <Destructing />
+            <FunctionDestructing />
+      </div>
+  );
+}
+export default JavaScript
