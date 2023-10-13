36b525bfccb691a0cbd8d80d17a8698272669e26
John-C-Kuang <kuang.che@northeastern.edu>
2023-10-13 00:30:57 -0400

    nav bar done

--- src/Kanbas/KanbasNavigation/index.js
+++ src/Kanbas/KanbasNavigation/index.js
@@ -1,32 +1,41 @@
 import { Link, useLocation } from "react-router-dom";
 import React from "react";
 import iconMap from "./iconMap";
-import "../style"
+import styles from '../style';
 
 function KanbasNavigation() {
   const links = [
-      " ",
-      "Account",
-      "Dashboard",
-      "Courses",
-      "Calendar",
-      "Inbox",
-      "History",
-      "Studio",
-      "Help",
+    " ",
+    "Account",
+    "Dashboard",
+    "Courses",
+    "Calendar",
+    "Inbox",
+    "History",
+    "Studio",
+    "Help",
   ];
   const { pathname } = useLocation();
   return (
       <div className="list-group" style={{ width: 150 }}>
-        {links.map((link, index) => (
-            <Link
-                key={index}
-                to={`/Kanbas/${link}`}
-                className={`list-group-item ${pathname.includes(link) && "active"}`}>
-              {iconMap[link]}
-              {link}
-            </Link>
-        ))}
+        {links.map((link, index) => {
+          const isActive = pathname.includes(link);
+          const combinedStyles = {
+            ...styles.parentNavLink,
+            color: isActive ? "red" : "white",
+          };
+          return (
+              <Link
+                  key={index}
+                  to={`/Kanbas/${link}`}
+                  className={`list-group-item p-2 border-0 ${isActive && "active"} ${isActive && "bg-white"}`}
+                  style={combinedStyles}>
+
+                {iconMap[link]}
+                {link}
+              </Link>
+          );
+        })}
       </div>
   );
 }
