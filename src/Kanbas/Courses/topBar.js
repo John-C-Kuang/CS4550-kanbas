9c022510a93019484d8d1865d8a2f633c6db91c8
John-C-Kuang <kuang.che@northeastern.edu>
2023-10-13 16:19:53 -0400

    save some work

---
+++ src/Kanbas/Courses/topBar.js
@@ -0,0 +1,17 @@
+const TopBar = ({course}) => (
+    <div className="row p-0 mt-3 mb-1 ms-2 align-items-center justify-content-center">
+      <div className="col-auto p-0">
+        <button className="btn btn-transparent p-2">
+          <div className="bg-danger my-1 neu-red" style={{width: '25px', height: '3px'}}></div>
+          <div className="bg-danger my-1 neu-red" style={{width: '25px', height: '3px'}}></div>
+          <div className="bg-danger my-1 neu-red" style={{width: '25px', height: '3px'}}></div>
+        </button>
+      </div>
+
+      <div className="col p-0">
+        <p className="my-0" style={{fontSize: "18px"}}>{course._id} · {course.name}</p>
+      </div>
+    </div>
+);
+
+export default TopBar
