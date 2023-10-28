ff1f6ec23a03e309df6a635d694582a5fd24926b
John-C-Kuang <kuang.che@northeastern.edu>
2023-10-28 18:14:50 -0400

    a4 begin redux

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -5,6 +5,14 @@ import PassingFunctions from "./PassingFunctions";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import EventObject from "./EventObject";
 import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ChildStateComponent from "./ChildStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
 
 const Assignment4 = () => {
@@ -15,6 +23,14 @@ const Assignment4 = () => {
   return(
       <>
         <h1>Assignment 4</h1>
+        <ReduxExamples/>
+        <ParentStateComponent/>
+        <ChildStateComponent/>
+        <ArrayStateVariable/>
+        <ObjectStateVariable/>
+        <DateStateVariable/>
+        <StringStateVariables/>
+        <BooleanStateVariables/>
         <Counter/>
         <EventObject/>
         <PassingFunctions theFunction={sayHello} />
